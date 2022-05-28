88357219247495a21d66a40f28673c45793391a1
zhengxinonly <pyxxponly@gmail.com>
2022-05-28 23:36:32 +0800

    添加初始化数据

--- models/rights.py
+++ models/rights.py
@@ -32,6 +32,22 @@ class RightModel(db.Model):
 
     parent = db.relationship("RightModel", remote_side=[id])  # 自关联
 
+    """
+    id = db.Column(db.Integer, primary_key=True, comment='权限编号')
+    powerName = db.Column(db.String(255), comment='权限名称')
+    powerType = db.Column(db.SMALLINT, comment='权限类型')
+    powerCode = db.Column(db.String(30), comment='权限标识')
+    powerUrl = db.Column(db.String(255), comment='权限路径')
+    openType = db.Column(db.String(10), comment='打开方式')
+    parentId = db.Column(db.Integer, db.ForeignKey("rt_power.id"), comment='父类编号')
+    icon = db.Column(db.String(128), comment='图标')
+    sort = db.Column(db.Integer, comment='排序')
+    enable = db.Column(db.Boolean, comment='是否开启')
+    checkArr = db.Column(db.String(128), comment='')
+
+    parent = db.relationship("RightModel", remote_side=[id])  # 自关联
+    """
+
 
 class RoleModel(db.Model):
     __tablename__ = 'rt_role'
