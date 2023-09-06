03ef6c681a608c43b0c9054caa51478d88d58dbb
losepure <651009587@qq,com>
2023-09-06 21:38:26 +0800

    为用户角色添加用户组功能，以便在角色较多时更清晰的分类和显示

--- applications/models/__init__.py
+++ applications/models/__init__.py
@@ -3,7 +3,7 @@ from .admin_dict import DictType, DictData
 from .admin_log import AdminLog
 from .admin_photo import Photo
 from .admin_power import Power
-from .admin_role import Role
+from .admin_role import Role,RoleGroup
 from .admin_role_power import role_power
 from .admin_user import User
 from .admin_user_role import user_role
--- applications/schemas/admin_role.py
+++ applications/schemas/admin_role.py
@@ -1,12 +1,37 @@
 from flask_marshmallow.sqla import SQLAlchemyAutoSchema
-
+from applications.extensions import ma
+from applications.extensions.init_sqlalchemy import db
+from marshmallow import fields
 from applications.models import Role
 
 
-class RoleOutSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Role  # table = models.Album.__table__
-        # include_relationships = True  # 输出模型对象时同时对外键，是否也一并进行处理
-        include_fk = True  # 序列化阶段是否也一并返回主键
-        # fields= ["id","name"] # 启动的字段列表
-        # exclude = ["id","name"] # 排除字段列表
+# class RoleOutSchema(SQLAlchemyAutoSchema):
+    # class Meta:
+    #     model = Role  # table = models.Album.__table__
+    #     include_relationships = True  # 输出模型对象时同时对外键，是否也一并进行处理
+    #     include_fk = True  # 序列化阶段是否也一并返回主键
+    #     sqla_session=db.session
+    #     # fields= ["id","name"] # 启动的字段列表
+    #     # exclude = ["id","name"] # 排除字段列表
+
+class RoleOutSchema(ma.Schema):
+    id = fields.Integer()
+    name = fields.Str()
+    code = fields.Str()
+    enable=fields.Integer()
+    remark = fields.Str()
+    details = fields.Str()
+    sort = fields.Integer()
+    create_time = fields.DateTime()
+    update_time = fields.DateTime()
+    group=fields.Nested(lambda:RoleGroupSchema())
+
+# 用户models的序列化类
+class RoleGroupSchema(ma.Schema):
+    id = fields.Integer()
+    name = fields.Str()
+    remark = fields.Str()
+    sort = fields.Integer()
+    create_time = fields.DateTime()
+    update_time = fields.DateTime()
+    # roles = fields.Nested(lambda: RoleOutSchema())
