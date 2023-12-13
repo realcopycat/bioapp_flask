e462c568ec125753ba7c0ea20ddfd24686429890
liuzhexian <1037512352@qq.com>
2023-12-13 17:26:31 +0800

    调整回适合自己的配置

--- applications/config.py
+++ applications/config.py
@@ -35,15 +35,15 @@ class BaseConfig:
     SECRET_KEY = "pear-system-flask"
 
     # mysql 配置
-    # MYSQL_USERNAME = "root"
-    # MYSQL_PASSWORD = "123456"
-    # MYSQL_HOST = "127.0.0.1"
-    # MYSQL_PORT = 3306
-    # MYSQL_DATABASE = "PearAdminFlask"
+    MYSQL_USERNAME = "root"
+    MYSQL_PASSWORD = "123456"
+    MYSQL_HOST = "127.0.0.1"
+    MYSQL_PORT = 3306
+    MYSQL_DATABASE = "PearAdminFlask"
 
     # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../pear.db'
-    # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USERNAME}:{urlquote(MYSQL_PASSWORD)}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}?charset=utf8mb4"
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///../pear.db'
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}?charset=utf8mb4"
 
     # 默认日志等级
     LOG_LEVEL = logging.WARN
